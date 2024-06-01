cc503b69d70cb701c0db710dee58efba3b67ba40
Lair Rico G <68044486+LairW3b@users.noreply.github.com>
2024-06-01 16:12:47 -0600

    comentarios agregados

--- src/components/Comentarios.jsx
+++ src/components/Comentarios.jsx
@@ -7,7 +7,7 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
 // import required modules
-import { Pagination, Navigation } from 'swiper/modules';
+import { Navigation } from 'swiper/modules';
 
 
 const Comentarios = () => {
@@ -25,10 +25,10 @@ const Comentarios = () => {
           <div className='swiper_content'>
             <div className='user'>
               <img src="/woman-user.svg" className='swiper_img' alt="user_icon" />
-              <p>Alejandro Perez P.</p>
+              <p> Monica Galindo.</p>
             </div>
             <p className='user_comment'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est unde delectus odit, quae animi dicta.
+              Decidí probar este servicio de restauración de faros y no podría estar más satisfecho.  Además, el precio fue muy razonable comparado con el costo de reemplazar los faros. ¡Definitivamente lo recomendaría a cualquiera!
             </p>
           </div>
         </SwiperSlide>
@@ -36,10 +36,21 @@ const Comentarios = () => {
           <div className='swiper_content'>
             <div className='user'>
               <img src="/user.svg" className='swiper_img' alt="user_icon" />
-              <p>Alejandro Perez P.</p>
+              <p>Alejandro Perez.</p>
             </div>
             <p className='user_comment'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est unde delectus odit, quae animi dicta.
+              ¡Servicio excelente! Llevé mi coche para pintar las facias y el resultado fue fantástico.
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='swiper_content'>
+            <div className='user'>
+              <img src="/woman-user.svg" className='swiper_img' alt="user_icon" />
+              <p>Paty Flores.</p>
+            </div>
+            <p className='user_comment'>
+              El acabado es impecable y se nota la atención al detalle. El personal fue muy amable y profesional. Además, el precio fue muy competitivo.
             </p>
           </div>
         </SwiperSlide>
@@ -47,10 +58,43 @@ const Comentarios = () => {
           <div className='swiper_content'>
             <div className='user'>
               <img src="/user.svg" className='swiper_img' alt="user_icon" />
-              <p>Alejandro Perez P.</p>
+              <p>Jesus Navarrete.</p>
             </div>
             <p className='user_comment'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est unde delectus odit, quae animi dicta.
+              El equipo fue muy profesional y el proceso fue rápido. Ahora mis faros lucen como nuevos y la diferencia en la visibilidad nocturna es impresionante.
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='swiper_content'>
+            <div className='user'>
+              <img src="/woman-user.svg" className='swiper_img' alt="user_icon" />
+              <p> Maria Jose Zarate.</p>
+            </div>
+            <p className='user_comment'>
+              Las facias tenían algunos rasguños y desgaste por el uso diario, pero ahora parecen completamente nuevas. ¡Super recomendable!
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='swiper_content'>
+            <div className='user'>
+              <img src="/user.svg" className='swiper_img' alt="user_icon" />
+              <p>Carlos Martinez.</p>
+            </div>
+            <p className='user_comment'>
+              ¡Impresionante servicio de baño de pintura!. Decidí darle un cambio de imagen con un baño de pintura y no podría estar más contento con el resultado. El equipo fue muy atento y cumplió con los plazos prometidos.
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='swiper_content'>
+            <div className='user'>
+              <img src="/woman-user.svg" className='swiper_img' alt="user_icon" />
+              <p>Irma Carreón.</p>
+            </div>
+            <p className='user_comment'>
+              ¡Buen servicio de pulido y encerado! Mi coche parecía opaco y con algunos rasguños, pero después del tratamiento luce como nuevo. El equipo hizo un buen trabajo y en el tiempo estipulado. Además, el precio fue muy razonable. ¡Altamente recomendado!
             </p>
           </div>
         </SwiperSlide>
